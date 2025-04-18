abd5138942b6875308d79de6e0f531f6bec9a5db
Abhay Ojha <abhayojha0012@gmail.com>
2025-04-18 15:51:03 +0530

    Modified Folder Structure

--- setOrg1.sh
+++ setOrg1.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# Dynamically get the base project path (where this script resides)
+BASE_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
 # Add Fabric binaries to PATH
-export PATH=$PATH:$HOME/Desktop/blockchain/fabric-samples/bin
+export PATH=$PATH:$BASE_DIR/fabric-samples/bin
 
-export FABRIC_CFG_PATH=$HOME/Desktop/blockchain/fabric-samples/config
+# Set FABRIC_CFG_PATH dynamically
+export FABRIC_CFG_PATH=$BASE_DIR/fabric-samples/config
 
-
-# Set Org1 environment
+# Set Org1 environment variables
 export CORE_PEER_TLS_ENABLED=true
 export CORE_PEER_LOCALMSPID="Org1MSP"
-export CORE_PEER_TLS_ROOTCERT_FILE=$HOME/Desktop/blockchain/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
-export CORE_PEER_MSPCONFIGPATH=$HOME/Desktop/blockchain/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_TLS_ROOTCERT_FILE=$BASE_DIR/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=$BASE_DIR/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
 export CORE_PEER_ADDRESS=localhost:7051
-export ORDERER_CA=$HOME/Desktop/blockchain/fabric-samples/test-network/organizations/ordererOrganizations/example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+export ORDERER_CA=$BASE_DIR/fabric-samples/test-network/organizations/ordererOrganizations/example.com/msp/tlscacerts/tlsca.example.com-cert.pem
